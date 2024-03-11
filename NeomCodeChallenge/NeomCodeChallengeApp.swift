45844a47d4420e648fe7729cf373d16217b62482
nf <nawaf.almuaither@ego-now.com>
2024-03-11 14:48:56 +0300

    Initial Commit

---
+++ NeomCodeChallenge/NeomCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeomCodeChallengeApp.swift
+//  NeomCodeChallenge
+//
+//  Created by Nawaf AlMuaither on 11/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct NeomCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
